1f3279c739cc8cb110d949ba6691fdccc5dadfb7
Shalaka <shalakaw17@gmail.com>
2025-12-28 16:00:45 +0530

    basic header features

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,27 @@ function App() {
             <h1>estore</h1>
           </div>
           <div className="inp-container p-0 my-4 w-50 h-25 bg-white">
+            <div className="dropdown m-0 p-0">
+              <select className='select-btn w-100 p-0 m-0' name="" id="">
+                <option value="">Men</option>
+                <option value="">Women</option>
+                <option value="">Kids</option>
+              </select>
+
+            </div>
             <input className="form-control" placeholder='Search...' />
             <button><i className="fa fa-search"></i></button>
+          </div>
+          <div className="login-container">
+            <i className="fa fa-user-circle user-icon"/>
+            <h5><a href="#">Login</a></h5> /
+            <h5><a href="#">Register</a></h5>
+          </div>
+          <div className="cart-wishlist">
+            <ul className='p-0'>
+              <li className="list-icon"><i className="fa fa-heart"></i></li>
+              <li className="list-icon"><i className="fa fa-shopping-cart"></i></li>
+            </ul>
           </div>
         </div>
       </div>
