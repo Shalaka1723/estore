d67c450d93125f98b47bcd1156ee5bc6f809fc0b
Shalaka <shalakaw17@gmail.com>
2025-12-28 15:39:20 +0530

    basic header

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,17 @@ function App() {
 
   return (
     <>
-      <div className='header bg-dark text-white'>
-        <h1>
-          this is something
-        </h1>
 
+      <div className='header bg-dark text-white'>
+        <div className="row">
+          <div className="brand my-1">
+            <h1>estore</h1>
+          </div>
+          <div className="inp-container p-0 my-4 w-50 h-25 bg-white">
+            <input className="form-control" placeholder='Search...' />
+            <button><i className="fa fa-search"></i></button>
+          </div>
+        </div>
       </div>
       
     </>
