b2ae12bc5f8f56e89b5add182b55e38796fdcb7f
Shalaka <shalakaw17@gmail.com>
2025-12-28 19:24:42 +0530

    category component basic

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import TopNav from './Components/TopNav/TopNav'
+import CatNav from './Components/CatNav/Index'
 
 function App() {
 
   return (
     <>
       <TopNav/>
+      <CatNav/>
     </>
   )
 }
