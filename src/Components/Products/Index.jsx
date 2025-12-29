6e9fb64ddf55fc2c9f4ecf0b1f222adb9cde08be
Shalaka <shalakaw17@gmail.com>
2025-12-29 23:07:25 +0530

    ui changes for product display from js dataset

--- src/Components/Products/Index.jsx
+++ src/Components/Products/Index.jsx
@@ -51,9 +51,10 @@ const Products = () => {
 
   return(
     <>
-    {
-      productData.map((product,key) => {
-        return(
+      <div className='products-container'>
+        {
+          productData.map((product,key) => {
+            return(
               <div className="mx-5 p-3 col-lg-3 col-md-6 product-card">
                 <div className='product-image-container'>
                   <img src={images[product.img]} alt="" />
@@ -61,12 +62,19 @@ const Products = () => {
                 <div className='product-info'>
                   <h5><a href="">{product.pName}</a></h5>
                   <p className='product-price'>${product.price}</p>
+                  <div className="product-rating">
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                  </div>
                 </div>
               </div>
-        )
-      })
-    }
-
+            )
+          })
+        }
+      </div>
     </>
   )
 }
